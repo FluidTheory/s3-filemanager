94b7403e6283ddb2277bbda9132d827e8ae7771a
sanjogkaskar <sanjog.kaskar@hnrtech.com>
2019-04-17 19:26:32 +0530

    Updated Package

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -2,20 +2,16 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>File Manager - S3 Integrated</title>
-
     <!-- Bootstrap core CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/filemanager-custom.css?v=1.2">
 
 </head>
-
 <body>
 
 <!-- Navigation -->
