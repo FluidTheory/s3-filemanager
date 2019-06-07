4538f97674b085179e981c774113afa95f2e1dac
sanjogkaskar <sanjog.kaskar@hnrtech.com>
2019-06-08 01:31:34 +0530

    [NR-1001] - Safari image upload issue

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,7 +7,7 @@
  */
 
 
-Route::group(['namespace' => 'Fluidtheory\Filemanager\Http\Controllers','scheme' => 'https'], function (){
+Route::group(['namespace' => 'Fluidtheory\Filemanager\Http\Controllers'], function (){
 
     Route::post('/delete_file', 'FileManagerController@delete');
     Route::post('/filemanager/upload', 'FileManagerController@upload');
