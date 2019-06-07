d3ed63341e9a481d4bb85867a07ebd9399b426ab
sanjogkaskar <sanjog.kaskar@hnrtech.com>
2019-06-07 19:59:56 +0530

    [NR-1005] - File manager updates

--- src/views/file-manager/iframe.blade.php
+++ src/views/file-manager/iframe.blade.php
@@ -1,5 +1,5 @@
 {{--css--}}
-<link rel="stylesheet" href="/css/filemanager.css?v=1.8">
+<link rel="stylesheet" href="/css/filemanager.css?v=1.9">
 {{--start model for S3 file manager --}}
 <div class="modal filemanager-iframe" id="fileManagerModal" tabindex="-1" role="dialog" aria-labelledby="fileManagerModalLabel"
      aria-hidden="true">
