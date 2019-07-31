903bb230dd08e63004d76cc71df6f528348a50a2
sanjogkaskar <sanjog.kaskar@hnrtech.com>
2019-07-31 14:04:16 +0530

    [Dashboard - 1133115678619026] - Loading issue in IE Edge 17

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -8,7 +8,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>File Manager - S3 Integrated </title>
 
     <!-- Bootstrap core CSS -->
